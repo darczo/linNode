606640047e838c689ef651274223f97e1506497d
darczo <dmatych@gmail.com>
2020-03-15 18:13:23 +0100

    Use built-in LED to inform about transmission

--- master.cpp
+++ master.cpp
@@ -3,12 +3,18 @@
 void setup()
 {
     Serial.begin(19200);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop()
 {
+    digitalWrite(LED_BUILTIN, HIGH);
     Serial.print("Hello");
+    digitalWrite(LED_BUILTIN, LOW);
     delay(2000);
+    digitalWrite(LED_BUILTIN, HIGH);
     Serial.print("world");
+    digitalWrite(LED_BUILTIN, LOW);
     delay(2000);
 }
--- slave.cpp
+++ slave.cpp
@@ -7,12 +7,17 @@ void setup()
 {
     Serial.begin(19200);    
     softSerial.begin(19200); 
+
+     pinMode(LED_BUILTIN, OUTPUT);
+     digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop()
 {
     if (Serial.available())
     {
+        digitalWrite(LED_BUILTIN, HIGH);
         softSerial.println((char)Serial.read());
-    }
+        digitalWrite(LED_BUILTIN, LOW);
+    }   
 }
